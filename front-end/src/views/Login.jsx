6a8fd9c3d0cf71f98bbb5390b266544329a8dcde
ccmendezt <camilomendez1903@gmail.com>
2023-04-26 10:12:20 -0500

    Inicio Front-end

---
+++ front-end/src/views/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import "../styles/login.css";
+
+const Login = () => {
+
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+    /*axios.get('http://localhost:5000/api/users')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });*/
+    //console.log(user);
+  };
+
+  const onSubmit = () => {
+    //Peticion a la API con Axios
+    axios.get('http://localhost:5000/api/users/')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <h1 className="titleIniciarSesion">Iniciar Sesion</h1>
+      <form>
+        <div className="row mb-3">
+          <label for="inputUsuario" className="col-sm-2 col-form-label">Usuariio</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputUsuario" />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label for="inputPassword" className="col-sm-2 col-form-label">Contraseña</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="inputPassword" onChange={ handleChange } />
+          </div>
+        </div>
+        <div className="btnLogin">
+          <button type="submit" className="btn btn-primary">Iniciar sesion</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
