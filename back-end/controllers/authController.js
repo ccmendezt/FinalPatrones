e29b4b5b0725e8676f009614c82bccc32a334357
ccmendezt <ccmendezt@udistrital.edu.co>
2023-05-23 00:11:06 -0500

    Generate password

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -3,25 +3,36 @@ const bcryptjs = require('bcryptjs');
 const connectionDB = require('../database/db');
 const serialize = require('cookie');
 const { promisify } = require('util');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const fs = require('fs');
+// const plantilla = fs.readFileSync('./templateRegister.ejs', 'utf-8'); // Carga y renderiza la plantilla de correo electrónico
 
 //Metodo para registrar un usuario
 exports.register = async (req, res) => {
   try {
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const usuario = req.body.usuario;
     const email = req.body.email;
-    const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
     const cardNumber = req.body.cardNumber;
-    if (password !== confirmPassword || password === "" || confirmPassword === "" || email === "" || cardNumber === "") {
-      console.log("Las contraseñas no coinciden");
-      res.status(500).send({ error: "Las contraseñas no coinciden" })
+    const passGenerate = generarPassword();
+    if (email === "" || nombre === "" || apellido === "" || usuario === "") {
+      res.status(500).send({ error: "Completa todos los campos" })
     } else {
-      let passHash = await bcryptjs.hash(password, 8);
-      connectionDB.query("INSERT INTO users (email, password, creditCard) VALUES (?, ?, ?)", [email, passHash, cardNumber], (err, result) => {
-        if (err) {console.log(err);} else {res.send(result);}
+      let idTarjeta = null;
+      if (cardNumber === "") {
+        idTarjeta = null;
+      }
+      let passHash = await bcryptjs.hash(passGenerate, 8);
+      connectionDB.query("INSERT INTO usuario (nombre, apellido, usuario, email, password, idRol, idTarjeta) VALUES (?, ?, ?, ?, ?, ?, ?)", [nombre, apellido, usuario, email, passHash, '2', idTarjeta], (err, result) => {
+        if (err) { console.log(err); }
       })
+      enviarEmail(nombre, apellido, email, usuario, passGenerate);//Envio de correo
       res.send("Usuario registrado correctamente")
     }
-  } catch (error) {console.log(error);}
+  } catch (error) { console.log(error); }
 }
 
 //Metodo para logear un usuario
@@ -29,7 +40,7 @@ exports.login = async (req, res) => {
   try {
     const email = req.body.email;
     const password = req.body.password;
-    const sql = 'SELECT * FROM users WHERE email = ?';
+    const sql = 'SELECT * FROM usuario WHERE email = ?';
 
     if (!email || !password) {
       return res.status(400).send({ error: "Introduce un email y una contraseña" });
@@ -54,8 +65,8 @@ exports.login = async (req, res) => {
             //   httpOnly: true
             // }
             //console.log("El token es: " + token + " para el usuario " + email + " con id " + id + "");
-            //res.cookie("jwt" , token, cookieOptions);
-            res.status(200).send({ message: 'Inicio de sesión exitoso', token});
+            //res.cookie("jwt" , token, cookieOptions);)
+            res.status(200).send({ message: 'Inicio de sesión exitoso', token });
             //res.send('¡Cookie establecida!');
           }
         }
@@ -69,7 +80,7 @@ exports.login = async (req, res) => {
 //Metodo para obtener todos los usuarios
 exports.getAllUsers = async (req, res) => {
   try {
-    const sql = 'SELECT * FROM users';
+    const sql = 'SELECT * FROM usuario';
     connectionDB.query(sql, (err, result) => {
       if (err) { console.log(err) };
       res.send(result)
@@ -77,4 +88,63 @@ exports.getAllUsers = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+}
+
+function generarPassword() {
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const longitud = 8;
+  let password = '';
+
+  while (password.length < longitud || !contieneRequisitos(password)) {
+    password = '';
+    for (let i = 0; i < longitud; i++) {
+      const indice = crypto.randomInt(0, caracteres.length);
+      password += caracteres[indice];
+    }
+  }
+
+  return password;
+}
+
+function contieneRequisitos(password) {
+  return /\d/.test(password) && /[A-Z]/.test(password) && /[a-z]/.test(password);
+}
+
+function enviarEmail(nombre, apellido, email, usuario, password) {
+  // Configuración del transporte de correo
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'parkudcolombia@gmail.com',
+      pass: 'dmqgugraaponotaz'
+    }
+  });
+  
+  const datos = {
+    nombre: nombre,
+    apellido: apellido,
+    usuario: usuario,
+    password: password
+  };
+
+  console.log(datos);
+
+  // const contenidoCorreo = ejs.render(plantilla, datos);
+
+  // Definición del mensaje de correo con el contenido personalizado
+  const mensajeCorreo = {
+    from: 'parkudcolombia@gmail.com',
+    to: email,
+    subject: '¡Registro Exitoso! - ParkUD Colombia',
+    text: 'Este es un mensaje de prueba enviado desde Node.js con sendmail.'
+  };
+
+  // Envío del correo electrónico
+  transporter.sendMail(mensajeCorreo, function (error, info) {
+    if (error) {
+      console.log('Error al enviar el correo electrónico:', error);
+    } else {
+      console.log('Correo electrónico enviado:', info.response);
+    }
+  });
 }
--- front-end/src/views/Register.jsx
+++ front-end/src/views/Register.jsx
@@ -7,30 +7,33 @@ import Logo from '../images/Logo.PNG'
 
 function Register() {
 
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [usuario, setUsuario] = useState('');
   const [cardNumber, setCardNumber] = useState('');
   const [message, setMessage] = useState('');
   const [registrado, setRegistrado] = useState(false);
 
   const handleSignup = async () => {
-    try{
+    try {
       const response = await axios.post('http://localhost:5000/api/users/register', {
+        nombre,
+        apellido,
+        usuario,
         email,
-        password,
-        confirmPassword,
         cardNumber
       });
       if (response.status === 200) {
         setMessage('Usuario registrado con éxito');
+        setNombre('');
+        setApellido('');
+        setUsuario('');
         setEmail('');
-        setPassword('');
-        setConfirmPassword('');
         setCardNumber('');
         setRegistrado(true);
       }
-    }catch(e){
+    } catch (e) {
       console.log(e.response.data);
     }
   };
@@ -39,31 +42,34 @@ function Register() {
     console.log(Cookies.get('jwt'));
   }
 
-  return (  
+  return (
 
     <div>
-      {registrado ? 
-          <Link to="/">
-            <button>Regresar</button>
-          </Link>
-        : 
-          <div>
-            <div id="logo">
-              <img src={Logo} style={{width:'auto'}}  alt="Logotipo"/>
-            </div>
-            <h1>Registro</h1>
-            <form onSubmit={(e) => e.preventDefault()}>
-                <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Correo electrónico'/>
-                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Contraseña'/>
-                <input type="password" id="confirmpassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Confirmar Contraseña'/>
-                <input type='text' id='cardNumber' value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} placeholder='Número de tarjeta'/>
-              <button onClick={handleSignup} className="btn" >Registrarse</button>
-              <button onClick={handleCookie} className="btn" >Obtener Cookie</button>
-            </form>
-            {<div>{message}</div>}
+      {registrado ?
+        <Link to="/">
+          <button>Regresar</button>
+        </Link>
+        :
+        <div>
+          <div id="logo">
+            <img src={Logo} style={{ width: 'auto' }} alt="Logotipo" />
           </div>
-        }
-        
+          <h1>Registro</h1>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} placeholder='Nombre' />
+            <input type="text" id="apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} placeholder='Apellido' />
+            <input type="text" id="usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} placeholder='Usuario' />
+            <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Correo electrónico' />
+            {/* <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Contraseña' />
+            <input type="password" id="confirmpassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Confirmar Contraseña' /> */}
+            <input type='text' id='cardNumber' value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} placeholder='Número de tarjeta' />
+            <button onClick={handleSignup} className="btn" >Registrarse</button>
+            <button onClick={handleCookie} className="btn" >Obtener Cookie</button>
+          </form>
+          {<div>{message}</div>}
+        </div>
+      }
+
     </div>
   );
 }
